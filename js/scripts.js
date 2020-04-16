acc0682a8731192560268f900b1c85e7ed4f2463
Joydeep Sengupta <joydeep.roni@gmail.com>
2020-04-16 16:55:54 +0300

    added greeting

--- js/scripts.js
+++ js/scripts.js
@@ -65,6 +65,26 @@
 		  }
 		});
 
+		// GREETING
+
+		var thehours = new Date().getHours();
+	var themessage;
+	var morning = ('Good morning!');
+	var afternoon = ('Good afternoon!');
+	var evening = ('Good evening!');
+
+	if (thehours >= 0 && thehours < 12) {
+		themessage = morning;
+
+	} else if (thehours >= 12 && thehours < 16) {
+		themessage = afternoon;
+
+	} else if (thehours >= 16 && thehours < 24) {
+		themessage = evening;
+	}
+
+	$('.greeting').append(themessage);
+
 
 
 		// CONTACT FORM INPUT LABEL
